f8488e5f8810d12052510c37d00a887b8ba75c80
shinebayar-g <3091558+shinebayar-g@users.noreply.github.com>
2021-09-07 01:03:16 -0400

    Handle multiple container shutdowns correctly, improve reliability, fix #24

--- main.go
+++ main.go
@@ -9,9 +9,20 @@ import (
 	"strings"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/events"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 )
+
+type ufwSource struct {
+	CIDR    string
+	comment string
+}
+
+type ufwEvent struct {
+	container types.ContainerJSON
+	msg       events.Message
+}
 
 func checkIP(ip string) bool {
 	return net.ParseIP(ip) != nil
@@ -22,9 +33,66 @@ func checkCIDR(cidr string) bool {
 	return err == nil
 }
 
-type ufwSource struct {
-	CIDR    string
-	comment string
+func isUfwManaged(containerLabel string) bool {
+	return strings.ToUpper(containerLabel) == "TRUE"
+}
+
+func handleUfwRule(ch <-chan ufwEvent) {
+	for event := range ch {
+		for port, portMaps := range event.container.NetworkSettings.Ports {
+			// List is non empty if port is published
+			if len(portMaps) > 0 {
+				ufwSourceList := []ufwSource{}
+				if event.msg.Actor.Attributes["UFW_ALLOW_FROM"] != "" {
+					ufwAllowFromList := strings.Split(event.msg.Actor.Attributes["UFW_ALLOW_FROM"], ";")
+
+					for _, allowFrom := range ufwAllowFromList {
+						ip := strings.Split(allowFrom, "-")
+
+						if !checkIP(ip[0]) {
+							if !checkCIDR(ip[0]) {
+								fmt.Printf("ufw-docker-automated: address %s is not valid!\n", ip[0])
+								continue
+							}
+						}
+
+						if len(ip) > 1 {
+							ufwSourceList = append(ufwSourceList, ufwSource{CIDR: ip[0], comment: fmt.Sprintf(" %s", ip[1])})
+						} else {
+							ufwSourceList = append(ufwSourceList, ufwSource{CIDR: ip[0]})
+						}
+					}
+				} else {
+					ufwSourceList = append(ufwSourceList, ufwSource{CIDR: "any"})
+				}
+
+				for _, source := range ufwSourceList {
+					var cmd *exec.Cmd
+					if event.msg.Action == "start" {
+						cmd = exec.Command("ufw", "route", "allow", "proto", port.Proto(), "from", source.CIDR, "to", event.container.NetworkSettings.IPAddress, "port", port.Port(), "comment", event.msg.Actor.Attributes["name"]+":"+event.msg.ID[:12]+source.comment)
+						fmt.Println("ufw-docker-automated: Adding rule:", cmd)
+					} else {
+						cmd = exec.Command("ufw", "route", "delete", "allow", "proto", port.Proto(), "from", source.CIDR, "to", event.container.NetworkSettings.IPAddress, "port", port.Port(), "comment", event.msg.Actor.Attributes["name"]+":"+event.msg.ID[:12]+source.comment)
+						fmt.Println("ufw-docker-automated: Deleting rule:", cmd)
+					}
+					var stdout, stderr bytes.Buffer
+					cmd.Stdout = &stdout
+					cmd.Stderr = &stderr
+					err := cmd.Run()
+
+					if err != nil || stderr.String() != "" {
+						fmt.Println("ufw:", err, stderr.String())
+					} else {
+						fmt.Println("ufw:", stdout.String())
+					}
+				}
+				// ufw route allow proto tcp from any to 172.17.0.2 port 80 comment "Comment"
+				// ufw route allow proto <tcp|udp> <source> to <container_ip> port <port> comment <comment>
+				// ufw route delete allow proto tcp from any to 172.17.0.2 port 80 comment "Comment"
+				// ufw route delete allow proto <tcp|udp> <source> to <container_ip> port <port> comment <comment>
+			}
+		}
+	}
 }
 
 func main() {
@@ -40,120 +108,32 @@ func main() {
 
 	messages, errors := cli.Events(ctx, types.EventsOptions{Filters: filter})
 
+	ch := make(chan ufwEvent)
+	go handleUfwRule(ch)
+
 	for {
 		select {
 		case msg := <-messages:
 			if msg.Type == "container" && msg.Action == "start" {
-
-				if ufwManaged := msg.Actor.Attributes["UFW_MANAGED"]; strings.ToUpper(ufwManaged) == "TRUE" {
+				if ufwManaged := msg.Actor.Attributes["UFW_MANAGED"]; isUfwManaged(ufwManaged) {
 					container, err := cli.ContainerInspect(ctx, msg.ID)
 					if err != nil {
 						fmt.Println("ufw-docker-automated: Couldn't inspect container:", err)
 						continue
 					}
-
-					for port, portMaps := range container.NetworkSettings.Ports {
-						// List is non empty if port is published
-						if len(portMaps) > 0 {
-							ufwSourceList := []ufwSource{}
-							if msg.Actor.Attributes["UFW_ALLOW_FROM"] != "" {
-								ufwAllowFromList := strings.Split(msg.Actor.Attributes["UFW_ALLOW_FROM"], ";")
-
-								for _, allowFrom := range ufwAllowFromList {
-									ip := strings.Split(allowFrom, "-")
-
-									if !checkIP(ip[0]) {
-										if !checkCIDR(ip[0]) {
-											fmt.Printf("ufw-docker-automated: address %s is not valid!\n", ip[0])
-											continue
-										}
-									}
-
-									if len(ip) > 1 {
-										ufwSourceList = append(ufwSourceList, ufwSource{CIDR: ip[0], comment: fmt.Sprintf(" %s", ip[1])})
-									} else {
-										ufwSourceList = append(ufwSourceList, ufwSource{CIDR: ip[0]})
-									}
-								}
-							} else {
-								ufwSourceList = append(ufwSourceList, ufwSource{CIDR: "any"})
-							}
-
-							for _, source := range ufwSourceList {
-								var stdout, stderr bytes.Buffer
-								cmd := exec.Command("ufw", "route", "allow", "proto", port.Proto(), "from", source.CIDR, "to", container.NetworkSettings.IPAddress, "port", port.Port(), "comment", msg.Actor.Attributes["name"]+":"+msg.ID[:12]+source.comment)
-								fmt.Println("ufw-docker-automated: Adding rule:", cmd)
-								cmd.Stdout = &stdout
-								cmd.Stderr = &stderr
-								err := cmd.Run()
-
-								if err != nil || stderr.String() != "" {
-									fmt.Println("ufw:", err, stderr.String())
-								} else {
-									fmt.Println("ufw:", stdout.String())
-								}
-							}
-							// ufw route allow proto tcp from any to 172.17.0.2 port 80 comment "Comment"
-							// ufw route allow proto <tcp|udp> <source> to <container_ip> port <port> comment <comment>
-						}
-					}
+					ch <- ufwEvent{container, msg}
 				}
 			}
 			// We cannot get container network details once it's stopped, So
 			// we're deleting ufw rules as soon as container receives stop signal before it's stopped.
 			if msg.Type == "container" && msg.Action == "kill" {
-				if ufwManaged := msg.Actor.Attributes["UFW_MANAGED"]; strings.ToUpper(ufwManaged) == "TRUE" {
+				if ufwManaged := msg.Actor.Attributes["UFW_MANAGED"]; isUfwManaged(ufwManaged) {
 					container, err := cli.ContainerInspect(ctx, msg.ID)
 					if err != nil {
 						fmt.Println("ufw-docker-automated: Couldn't inspect container:", err)
 						continue
 					}
-
-					for port, portMaps := range container.NetworkSettings.Ports {
-						// List is non empty if port is published
-						if len(portMaps) > 0 {
-							ufwSourceList := []ufwSource{}
-							if msg.Actor.Attributes["UFW_ALLOW_FROM"] != "" {
-								ufwAllowFromList := strings.Split(msg.Actor.Attributes["UFW_ALLOW_FROM"], ";")
-
-								for _, allowFrom := range ufwAllowFromList {
-									ip := strings.Split(allowFrom, "-")
-
-									if !checkIP(ip[0]) {
-										if !checkCIDR(ip[0]) {
-											fmt.Printf("ufw-docker-automated: address %s is not valid!\n", ip[0])
-											continue
-										}
-									}
-
-									if len(ip) > 1 {
-										ufwSourceList = append(ufwSourceList, ufwSource{CIDR: ip[0], comment: fmt.Sprintf(" %s", ip[1])})
-									} else {
-										ufwSourceList = append(ufwSourceList, ufwSource{CIDR: ip[0]})
-									}
-								}
-							} else {
-								ufwSourceList = append(ufwSourceList, ufwSource{CIDR: "any"})
-							}
-
-							for _, source := range ufwSourceList {
-								var stdout, stderr bytes.Buffer
-								cmd := exec.Command("ufw", "route", "delete", "allow", "proto", port.Proto(), "from", source.CIDR, "to", container.NetworkSettings.IPAddress, "port", port.Port(), "comment", msg.Actor.Attributes["name"]+":"+msg.ID[:12]+source.comment)
-								fmt.Println("ufw-docker-automated: Deleting rule:", cmd)
-								cmd.Stdout = &stdout
-								cmd.Stderr = &stderr
-								err := cmd.Run()
-
-								if err != nil || stderr.String() != "" {
-									fmt.Println("ufw:", err, stderr.String())
-								} else {
-									fmt.Println("ufw:", stdout.String())
-								}
-							}
-							// ufw route delete allow proto tcp from any to 172.17.0.2 port 80 comment "Comment"
-							// ufw route delete allow proto <tcp|udp> <source> to <container_ip> port <port> comment <comment>
-						}
-					}
+					ch <- ufwEvent{container, msg}
 				}
 			}
 		case err := <-errors:
