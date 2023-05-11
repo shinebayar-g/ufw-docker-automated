03ca2f26b9bfec381bb380a56f245811e1e7efdc
Kushal Azim Ekram <kforeverisback@gmail.com>
2023-05-10 17:40:22 -0700

    Implemented Multi docker network and connection

--- ufwhandler/delete.go
+++ ufwhandler/delete.go
@@ -15,55 +15,61 @@ func DeleteUfwRule(containerID <-chan string, c *cache.Cache) {
 			container := cachedContainer.(*TrackedContainer)
 			// Handle inbound rules
 			for _, rule := range container.UfwInboundRules {
-				cmd := exec.Command("sudo", "ufw", "route", "delete", "allow", "proto", rule.Proto, "from", rule.CIDR, "to", container.IPAddress, "port", rule.Port, "comment", container.Name+":"+id+rule.Comment)
-				log.Info().Msg("ufw-docker-automated: Deleting inbound rule: " + cmd.String())
+				for dnetwork, containerIP := range container.IPAddressMap {
+					cmd := exec.Command("sudo", "ufw", "route", "delete", "allow", "proto", rule.Proto, "from", rule.CIDR, "to", containerIP, "port", rule.Port, "comment", container.Name+":"+id+rule.Comment)
+					log.Info().Msg("ufw-docker-automated: Deleting inbound rule (docker network :" + dnetwork + "): " + cmd.String())
 
-				var stdout, stderr bytes.Buffer
-				cmd.Stdout = &stdout
-				cmd.Stderr = &stderr
-				err := cmd.Run()
+					var stdout, stderr bytes.Buffer
+					cmd.Stdout = &stdout
+					cmd.Stderr = &stderr
+					err := cmd.Run()
 
-				if err != nil || stderr.String() != "" {
-					log.Error().Err(err).Msg("ufw error: " + stderr.String())
-				} else {
-					log.Info().Msg("ufw: " + stdout.String())
+					if err != nil || stderr.String() != "" {
+						log.Error().Err(err).Msg("ufw error: " + stderr.String())
+					} else {
+						log.Info().Msg("ufw: " + stdout.String())
+					}
 				}
 			}
 			// Handle outbound rules
 			for _, rule := range container.UfwOutboundRules {
-				var cmd *exec.Cmd
-				if rule.Port == "" {
-					cmd = exec.Command("sudo", "ufw", "route", "delete", "allow", "from", container.IPAddress, "to", rule.CIDR, "comment", container.Name+":"+id+rule.Comment)
-				} else {
-					cmd = exec.Command("sudo", "ufw", "route", "delete", "allow", "from", container.IPAddress, "to", rule.CIDR, "port", rule.Port, "comment", container.Name+":"+id+rule.Comment)
-				}
-				log.Info().Msg("ufw-docker-automated: Deleting outbound rule: " + cmd.String())
+				for dnetwork, containerIP := range container.IPAddressMap {
+					var cmd *exec.Cmd
+					if rule.Port == "" {
+						cmd = exec.Command("sudo", "ufw", "route", "delete", "allow", "from", containerIP, "to", rule.CIDR, "comment", container.Name+":"+id+rule.Comment)
+					} else {
+						cmd = exec.Command("sudo", "ufw", "route", "delete", "allow", "from", containerIP, "to", rule.CIDR, "port", rule.Port, "comment", container.Name+":"+id+rule.Comment)
+					}
+					log.Info().Msg("ufw-docker-automated: Deleting outbound rule (docker network :" + dnetwork + "): " + cmd.String())
 
-				var stdout, stderr bytes.Buffer
-				cmd.Stdout = &stdout
-				cmd.Stderr = &stderr
-				err := cmd.Run()
+					var stdout, stderr bytes.Buffer
+					cmd.Stdout = &stdout
+					cmd.Stderr = &stderr
+					err := cmd.Run()
 
-				if err != nil || stderr.String() != "" {
-					log.Error().Err(err).Msg("ufw error: " + stderr.String())
-				} else {
-					log.Info().Msg("ufw: " + stdout.String())
+					if err != nil || stderr.String() != "" {
+						log.Error().Err(err).Msg("ufw error: " + stderr.String())
+					} else {
+						log.Info().Msg("ufw: " + stdout.String())
+					}
 				}
 			}
 			// Handle deny all out
 			if container.Labels["UFW_DENY_OUT"] == "TRUE" {
-				cmd := exec.Command("sudo", "ufw", "route", "delete", "deny", "from", container.IPAddress, "to", "any", "comment", container.Name+":"+id)
-				log.Info().Msg("ufw-docker-automated: Deleting outbound rule: " + cmd.String())
+				for dnetwork, containerIP := range container.IPAddressMap {
+					cmd := exec.Command("sudo", "ufw", "route", "delete", "deny", "from", containerIP, "to", "any", "comment", container.Name+":"+id)
+					log.Info().Msg("ufw-docker-automated: Deleting outbound rule (docker network :" + dnetwork + "): " + cmd.String())
 
-				var stdout, stderr bytes.Buffer
-				cmd.Stdout = &stdout
-				cmd.Stderr = &stderr
-				err := cmd.Run()
+					var stdout, stderr bytes.Buffer
+					cmd.Stdout = &stdout
+					cmd.Stderr = &stderr
+					err := cmd.Run()
 
-				if err != nil || stderr.String() != "" {
-					log.Error().Err(err).Msg("ufw error: " + stderr.String())
-				} else {
-					log.Info().Msg("ufw: " + stdout.String())
+					if err != nil || stderr.String() != "" {
+						log.Error().Err(err).Msg("ufw error: " + stderr.String())
+					} else {
+						log.Info().Msg("ufw: " + stdout.String())
+					}
 				}
 			}
 		} else {
